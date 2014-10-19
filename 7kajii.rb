22d9d1659e09a9dd2674826d2a0c06b5f8176572
yaginuuu <yaginuuu56@gmail.com>
2014-10-20 01:18:40 +0900

    first commit

---
+++ 7kajii.rb
@@ -0,0 +1,40 @@
+require "timeout"
+
+hoshi = "sh hoshi.sh"
+beer = "sh beer.sh"
+msg = "say 'なかじー先生！お誕生日おめでとう！'"
+
+def kill(cmd)
+  pid = spawn(cmd, :pgroup => true)  # :pgroup => trueを追加
+  thr = Process.detach(pid)
+  begin
+    Timeout.timeout(5) do
+      thr.join
+    end
+  rescue Timeout::Error
+    puts "execution expired"
+    Process.kill(:TERM, -pid)        # -pidに変更
+  end
+end
+
+kill(hoshi)
+kill(beer)
+
+system("say '25歳ですね'")
+puts "\n"
+puts "\n"
+puts "\n"
+puts "\n"
+puts "\n"
+puts "\n"
+puts "\n"
+puts "┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈☆☆☆☆☆☆☆☆☆┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+      ┈┈┈┈┈┈┈┈┈┈╭┻┻┻┻┻┻┻┻┻╮┈┈┈┈┈┈┈┈┈┈
+      ┈┈┈┈┈┈┈┈┈┈┃╱╲╱╲╱╲╱╲╱┃┈┈┈┈┈┈┈┈┈┈
+      ┈┈┈┈┈┈┈┈┈╭┻━━━━━━━━━┻╮┈┈┈┈┈┈┈┈┈
+      ┈┈┈┈┈┈┈┈┈┃╱╲╱╲╱╲╱╲╱╲╱┃┈┈┈┈┈┈┈┈┈
+      ┈┈┈┈┈┈┈┈┈┗━━━━━━━━━━━┛┈┈┈┈┈┈┈┈┈
+      ┈┈┈┈┈┈┈┈HAPPY BIRTHDAY!┈┈┈┈┈┈┈┈
+      ┈┈┈┈┈┈┈┈@7kajii => 25age!┈┈┈┈┈┈"
+
+system(msg)
